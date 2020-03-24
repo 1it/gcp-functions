edd790941a959b1ad8e5c36120da88f5cf1ce0ee
Ivan Tuzhilkin <i.tuzhilkin@kefirgames.com>
2020-03-24 16:48:38 +0400

    Env check | Updated function behavior

--- lib/env_check.sh
+++ lib/env_check.sh
@@ -3,14 +3,17 @@
 TF_VERSION=${TF_VERSION:-0.12.23}
 
 if ! which terraform > /dev/null; then
-	echo -e "Error: terraform is not installed. Downloading ... \n"
-	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-	    wget https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_darwin_amd64.zip
-	    sudo unzip -uo unzip terraform_${TF_VERSION}_darwin_amd64.zip -d /usr/local/bin/
-	elif [[ "$OSTYPE" == "darwin"* ]]; then
-		wget https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
-		sudo unzip -uo terraform_${TF_VERSION}_linux_amd64.zip -d /usr/local/bin/
-	fi
+    echo -e "Error: terraform is not installed. \n"
+    echo -e "Proceed to download and install Terraform v$TF_VERSION?"
+    read -t 60 -p "Press [Enter] to proceed or [Ctrl]-[C] to exit"
+    echo -e "Downloading ... \n"
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        wget https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_darwin_amd64.zip
+        sudo unzip -uo unzip terraform_${TF_VERSION}_darwin_amd64.zip -d /usr/local/bin/
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        wget https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
+        sudo unzip -uo terraform_${TF_VERSION}_linux_amd64.zip -d /usr/local/bin/
+    fi
 fi
 
 if ! which gcloud > /dev/null; then echo "Error: gcloud is not installed" && exit 1; fi
